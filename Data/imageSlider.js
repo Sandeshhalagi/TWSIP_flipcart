bb0e8c6744aa5c0ba2e259588c5883927cbbcdbd
prajwalkotagi06 <prajwalkotagi2002@gmail.com>
2023-09-24 15:34:10 +0530

    first

---
+++ Data/imageSlider.js
@@ -0,0 +1,22 @@
+export const imageSlider = [
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/4529fd434a85c683.jpg?q=20",
+        link : "#"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/89c26b04d640c7a9.jpg?q=20",
+        link : "#"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/d02b3b49e4150abb.jpg?q=50",
+        link : "#"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/1688/280/image/c0bec65432fb07fc.jpg?q=50",
+        link : "#"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/c36f23a9e897bb96.jpg?q=20",
+        link : "#"
+    }
+]
